9396b5a7d36a86376c06120fba2fc41326f59c28
Vignan <35171316+Vignan18@users.noreply.github.com>
2023-06-19 16:39:33 +0530

    UserList Testing

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -7,13 +7,26 @@ test('render one rows per user',()=>{
         {name:'b',email:"b@gmail.com"}
 
     ]
-    render(<UserList users={users}/>)
+    // const {container} =
+     render(<UserList users={users}/>)
     // screen.logTestingPlaygroundURL();
     const rows = within(screen.getByTestId('users')).getAllByRole('row');
+    // const rows  = container.querySelectorAll('tbody tr');
     expect(rows).toHaveLength(2);
 
 })
 
 test('render the email and name of each user',()=>{
+    const users = [
+        {name:'a',email:"a@gmail.com"},
+        {name:'b',email:"b@gmail.com"}
+    ]
+    render(<UserList users={users}/>);
+    for(let user of users){
+        const name = screen.getByRole('cell',{name:user.name})
+        const email = screen.getByRole('cell',{name:user.email})
+        expect(name).toBeInTheDocument();
+        expect(email).toBeInTheDocument();
+    }
 
 })
